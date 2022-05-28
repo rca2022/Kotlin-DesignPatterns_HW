58d8f84bff065be5243e43c584ac876a4c323a61
rca2022 <renz-k.alfonso@gcash.com>
2022-05-28 17:55:51 +0800

    Added test cases

--- src/main/kotlin/creational/mediator/ChatRoom.kt
+++ src/main/kotlin/creational/mediator/ChatRoom.kt
@@ -5,6 +5,11 @@ import java.util.*
 class Chatroom {
     companion object {
         fun showMessage(user: User, message : String) =
-            "${Date()} [${user}] : ${message}"
+            if (user.toString().isEmpty()){
+                "Please input name"
+            }
+            else{
+                "${Date()} [${user}] : ${message}"
+            }
     }
 }
